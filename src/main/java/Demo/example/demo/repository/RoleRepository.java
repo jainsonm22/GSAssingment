9d25245defdef54739eb55d4658a160f1b07d472
jainsonm22 <jainsonm22@gmail.com>
2023-06-21 18:28:25 +0530

    first Commit

---
+++ src/main/java/Demo/example/demo/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package Demo.example.demo.repository;
+
+import Demo.example.demo.models.ERole;
+import Demo.example.demo.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(ERole name);
+}
